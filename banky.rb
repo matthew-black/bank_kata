b6658b158cad2852be331f0956f0d0cb2852e209
Matthew Black <matthewallenblack@gmail.com>
2018-02-27 10:39:27 -0600

    Separate each digit printout's four strings into subarrays.

--- banky.rb
+++ banky.rb
@@ -4,15 +4,11 @@ File.open("test_digits.txt").each do |line|
   file_lines << line.chomp
 end
 
-# file_lines.delete_if { |line| line == ""}
+chunked = file_lines.each_slice(4).to_a
 
-file_lines.each do |line|
-  p line
-end
+# chunked.map.with_index do |line, i|
 
-file_lines.map do |line|
-
-end
+# end
 
 #  _
 # | |
